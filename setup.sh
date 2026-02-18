2b1c2181b67b0de37493523760a7feba5528a34a
Krzysztof Bąk <krzysztof.bak@rst.com.pl>
2026-02-18 15:19:18 +0100

    init

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -eufo pipefail
+
+echo "🚀  Setting up dotfiles."
+
+if xcode-select -p &> /dev/null; then
+  echo "✅  Xcode command line tools are already installed."
+else
+  echo "🔧  Installing Xcode command line tools..."
+  xcode-select --install &> /dev/null
+  
+  while ! xcode-select -p &> /dev/null; do
+    sleep 5
+  done
+  echo "✅  Xcode command line tools installed successfully."
+fi
+
+if which -s "brew"; then
+  echo "✅  Homebrew is already installed."
+else
+  echo "🍺  Installing Homebrew"
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo "✅  Homebrew installed successfully."
+fi
+
+if which -s "chezmoi"; then
+  echo "✅  Chezmoi is already installed."
+else
+  echo "⚪️  Installing Chezmoi"
+  brew install chezmoi
+fi
